0fca4fd768f0a9d84ba902366d2ca7c3c9080f24
Yashvardhan Singh <yash2212003@gmail.com>
2023-08-26 00:26:37 +0530

    added changes

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#155DE9',
+        'secondary': '#F3B246',
+      }
+    },
+  },
+  plugins: [],
+}
+
